1ae6883dcb857d8703733ef3b2a900e6e25d2433
David Garner <dprgarner@gmail.com>
2020-10-18 01:08:55 +0100

    Add tabs for filtering games

--- src/client/LobbyPage/LobbyPage.tsx
+++ src/client/LobbyPage/LobbyPage.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
-import { Grid } from '@material-ui/core';
+import { Grid, Paper, Tab, Tabs } from '@material-ui/core';
 
 import FadeIn from '~/client/components/FadeIn';
 import MiscError from '../components/MiscError';
@@ -9,18 +9,65 @@ import useCreateMatch from './useCreateMatch';
 import SetLoadingBackdrop from '../context/loading/SetLoadingBackdrop';
 import MatchCard from './MatchCard';
 import useMatches from './useMatches';
+import { useProfile } from '../context/firebaseUser';
 
 const useStyles = makeStyles((theme) => ({
   lobby: {
     margin: theme.spacing(4),
   },
 
+  tabsPaper: {
+    marginBottom: theme.spacing(4),
+  },
+
   buttonContainer: {
-    marginBottom: theme.spacing(8),
+    marginBottom: theme.spacing(4),
     display: 'flex',
     justifyContent: 'center',
   },
 }));
+
+const hasCurrentPlayer = (
+  uid: string,
+  players: { data?: { uid: string } }[],
+) => {
+  for (const player of players) {
+    if (player.data?.uid === uid) {
+      return true;
+    }
+  }
+  return false;
+};
+
+const hasSpace = (players: { data?: { uid: string } }[]) => {
+  for (const player of players) {
+    if (!player.data) {
+      return true;
+    }
+  }
+  return false;
+};
+
+const partitionMatches = <M extends { players: { data?: { uid: string } }[] }>(
+  uid: string,
+  matches: M[],
+) => {
+  const playerMatches: M[] = [];
+  const openMatches: M[] = [];
+  const filledMatches: M[] = [];
+
+  matches.forEach((match) => {
+    if (hasCurrentPlayer(uid, match.players)) {
+      playerMatches.push(match);
+    } else if (hasSpace(match.players)) {
+      openMatches.push(match);
+    } else {
+      filledMatches.push(match);
+    }
+  });
+
+  return [playerMatches, openMatches, filledMatches];
+};
 
 const Lobby = () => {
   const classes = useStyles();
@@ -32,8 +79,15 @@ const Lobby = () => {
     }
   });
   const matches = useMatches();
+  const user = useProfile();
+  const [tab, setTab] = React.useState<0 | 1 | 2>(0);
 
-  if (!matches) return <SetLoadingBackdrop />;
+  if (!matches || !user) return <SetLoadingBackdrop />;
+
+  const [playerMatches, openMatches, filledMatches] = partitionMatches(
+    user.uid,
+    matches,
+  );
 
   return (
     <FadeIn>
@@ -49,8 +103,22 @@ const Lobby = () => {
           </Button>
         </div>
 
+        <Paper className={classes.tabsPaper}>
+          <Tabs
+            value={tab}
+            onChange={(_, idx) => setTab(idx)}
+            indicatorColor="primary"
+            textColor="primary"
+            centered
+          >
+            <Tab label="Your Games" />
+            <Tab label="Open Games" />
+            <Tab label="Filled Games" />
+          </Tabs>
+        </Paper>
+
         <Grid container spacing={4}>
-          {matches.map((match) => (
+          {[playerMatches, openMatches, filledMatches][tab].map((match) => (
             <MatchCard {...match} key={match.matchID} />
           ))}
         </Grid>
